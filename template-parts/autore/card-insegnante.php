5b7c2df36f8a94fbbd1f34c80f272ade1cf8cbd9
Marcella Monreale <marcella.monreale@sns.it>
2023-01-24 18:48:41 +0100

    In card-insegnante.php aggiunto nome struttura sotto nome e cognome

--- template-parts/autore/card-insegnante.php
+++ template-parts/autore/card-insegnante.php
@@ -15,6 +15,11 @@ if(!$disattiva_pagina_dettaglio) {
 ?>
 	<p class="font-weight-normal"><strong class="text-underline"><u><a href="<?php echo get_author_posts_url( $autore->ID);  ?>"><?php echo $nome . " " . $cognome; ?></a></u></strong></p>
 <?php
+	$terms = get_the_terms($ID, 'struttura');
+	$nome_struttura = $terms[0]->name;
+?>
+	<p class="font-weight-normal text-center"><strong><?php echo $nome_struttura; ?></strong></p>
+<?php
 } else {
 ?>
 	<p class="font-weight-normal"><strong><?php echo $nome . " " . $cognome; ?></strong></p>
