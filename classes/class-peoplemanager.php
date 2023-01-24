8cd5089f74a2c74005620f68e12986ac851eda4b
Marcella Monreale <marcella.monreale@sns.it>
2023-01-24 12:04:26 +0100

    Trasformata tassonomia Tipo Persona in content type in modo tale da poter associare una priorità ai tipi Aggiunto campo Categoria di appartenenza in custom type persona

--- classes/class-peoplemanager.php
+++ classes/class-peoplemanager.php
@@ -2,8 +2,7 @@
 // Post Types.
 define( 'PEOPLE_POST_TYPE', 'persona' );
 
-// Taxonomies
-define( 'PEOPLE_TYPE_TAXONOMY', 'tipo-persona' );
+define( 'PEOPLE_TYPE_POST_TYPE', 'tipologia-persona' );
 
 define ('STRUCTURE_TAXONOMY', 'struttura');
 
@@ -38,36 +37,6 @@ class People_Manager {
 	 * @return void
 	 */
 	public function add_taxonomies() {
-		//aggiungo la tassonomia tipo persona
-		$labels = array(
-			'name'              => _x( 'Tipologia Persona', 'taxonomy general name', 'design_laboratori_italia' ),
-			'singular_name'     => _x( 'Tipologia Persona', 'taxonomy singular name', 'design_laboratori_italia' ),
-			'search_items'      => __( 'Cerca Tipologia', 'design_laboratori_italia' ),
-			'all_items'         => __( 'Tutte le tipologie', 'design_laboratori_italia' ),
-			'edit_item'         => __( 'Modifica la Tipologia', 'design_laboratori_italia' ),
-			'update_item'       => __( 'Aggiorna la Tipologia', 'design_laboratori_italia' ),
-			'add_new_item'      => __( 'Aggiungi una Tipologia', 'design_laboratori_italia' ),
-			'new_item_name'     => __( 'Nuova Tipologia', 'design_laboratori_italia' ),
-			'menu_name'         => __( 'Tipologia', 'design_laboratori_italia' ),
-		);
-
-		$args = array(
-			'hierarchical'      => true,
-			'labels'            => $labels,
-			'show_ui'           => true,
-			'show_admin_column' => true,
-			'query_var'         => true,
-			'rewrite'           => array( 'slug' => 'tipologia-persona' ),
-			'capabilities'      => array(
-				'manage_terms' => 'manage_tipologia_persone',
-				'edit_terms'   => 'edit_tipologia_persone',
-				'delete_terms' => 'delete_tipologia_persone',
-				'assign_terms' => 'assign_tipologia_persone',
-			),
-		);
-
-		register_taxonomy( PEOPLE_TYPE_TAXONOMY, array( PEOPLE_POST_TYPE ), $args );
-
 		//aggiungo la tassonomia struttura
 
 		$structure_labels = array(
@@ -138,6 +107,36 @@ class People_Manager {
 
 		// Add the custom fields.
 		$this->add_fields();
+
+		//register post type Tipologia Persona
+
+		$labels = array(
+			'name'                  => _x( 'Tipologia Persone', 'Post Type General Name', 'design_laboratori_italia' ),
+			'singular_name'         => _x( 'Tipologia Persona', 'Post Type Singular Name', 'design_laboratori_italia' ),
+			'add_new'               => _x( 'Aggiungi Tipologia  Persona', 'Post Type Singular Name', 'design_laboratori_italia' ),
+			'add_new_item'          => _x( 'Aggiungi la Tipologia Persona', 'Post Type Singular Name', 'design_laboratori_italia' ),
+			'edit_item'             => _x( 'Modifica la Tipologia Persona', 'Post Type Singular Name', 'design_laboratori_italia' ),
+			'view_item'             => _x( 'Visualizza la Tipologia  Persona', 'Post Type Singular Name', 'design_laboratori_italia' )
+		);
+
+		$args   = array(
+			'label'           => __( 'Tipologia Persona', 'design_laboratori_italia' ),
+			'labels'          => $labels,
+			'supports'        => array( 'title'),
+			'public'          => true,
+			'show_in_menu'    => true,
+			'show_in_rest'    => true,
+			'menu_position'   => 2,
+			'menu_icon'       => 'dashicons-nametag',
+			'has_archive'     => true,
+			'show_in_rest'    => true,
+			'rewrite'         => array('slug' => 'tipologia-persone'),
+		);
+
+		register_post_type( PEOPLE_TYPE_POST_TYPE, $args );
+
+		// Add the custom fields.
+		$this->add_tipologia_fields();
 	}
 
 	/**
@@ -360,6 +359,32 @@ class People_Manager {
 						'placeholder' => 'https://',
 					),
 					array(
+						'key' => 'field_63cfb60b96994',
+						'label' => 'Categoria di appartenenza',
+						'name' => 'categoria_appartenenza',
+						'aria-label' => '',
+						'type' => 'relationship',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'post_type' => array(
+							0 => 'tipologia-persona',
+						),
+						'taxonomy' => '',
+						'filters' => array(
+							0 => 'search',
+						),
+						'return_format' => 'object',
+						'min' => '',
+						'max' => '',
+						'elements' => '',
+					),
+					array(
 						'key' => 'field_63c8152d68f0b',
 						'label' => 'Progetti correlati',
 						'name' => 'progetti_correlati',
@@ -472,7 +497,104 @@ class People_Manager {
 				'show_in_rest' => 1,
 			));
 			
+			
 		}
 	}
 
+	/**
+	 * Add the custom fields of the tipologia persona custom post-type.
+	 *
+	 * @return void
+	 */
+	function add_tipologia_fields() {
+		if( function_exists('acf_add_local_field_group') ) {
+
+			acf_add_local_field_group(array(
+				'key' => 'group_63cfb5b25e045',
+				'title' => 'Campi Tipologia Persona',
+				'fields' => array(
+					array(
+						'key' => 'field_63cfb5b3bd2e0',
+						'label' => 'Nome',
+						'name' => 'nome',
+						'aria-label' => '',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 1,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'maxlength' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+					array(
+						'key' => 'field_63cfb5c6bd2e1',
+						'label' => 'Descrizione',
+						'name' => 'descrizione',
+						'aria-label' => '',
+						'type' => 'text',
+						'instructions' => '',
+						'required' => 0,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'maxlength' => '',
+						'placeholder' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+					array(
+						'key' => 'field_63cfb5cdbd2e2',
+						'label' => 'Priorità',
+						'name' => 'priorita',
+						'aria-label' => '',
+						'type' => 'number',
+						'instructions' => '',
+						'required' => 1,
+						'conditional_logic' => 0,
+						'wrapper' => array(
+							'width' => '',
+							'class' => '',
+							'id' => '',
+						),
+						'default_value' => '',
+						'min' => 1,
+						'max' => '',
+						'placeholder' => '',
+						'step' => '',
+						'prepend' => '',
+						'append' => '',
+					),
+				),
+				'location' => array(
+					array(
+						array(
+							'param' => 'post_type',
+							'operator' => '==',
+							'value' => 'tipologia-persona',
+						),
+					),
+				),
+				'menu_order' => 0,
+				'position' => 'normal',
+				'style' => 'default',
+				'label_placement' => 'top',
+				'instruction_placement' => 'label',
+				'hide_on_screen' => '',
+				'active' => true,
+				'description' => '',
+				'show_in_rest' => 0,
+			));	
+		}
+	}
 }
