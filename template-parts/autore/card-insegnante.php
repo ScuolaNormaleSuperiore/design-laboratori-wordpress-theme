d6897a13cf4eec3160a39387f075562149acba70
Marcella Monreale <marcella.monreale@sns.it>
2023-01-24 10:35:18 +0100

    - Riscrittura della pagina persone - In utils.php definita funzione dsi_get_persona_avatar per ottenere l'avatar

--- template-parts/autore/card-insegnante.php
+++ template-parts/autore/card-insegnante.php
@@ -1,9 +1,12 @@
 <?php
-global $autore;
+global $nome;
+global $cognome;
+global $foto;
+global $ID;
 ?>
 <div class="card-avatar-img">
-	<a href="<?php echo get_author_posts_url( $autore->ID);  ?>"><img src="<?php echo dsi_get_user_avatar($autore); ?>" alt=""></a>
+	<a href="<?php echo get_author_posts_url( $autore->ID);  ?>"><img src="<?php echo dsi_get_persona_avatar($foto, $ID); ?>" alt="<?php echo $foto['alt']; ?>"></a>
 </div><!-- /card-avatar-img -->
 <div class="card-avatar-content">
-	<p class="font-weight-normal"><strong class="text-underline"><u><a href="<?php echo get_author_posts_url( $autore->ID);  ?>"><?php echo dsi_get_display_name($autore->ID); ?></a></u></strong></p>
+	<p class="font-weight-normal"><strong class="text-underline"><u><a href="<?php echo get_author_posts_url( $autore->ID);  ?>"><?php echo $nome . " " . $cognome; ?></a></u></strong></p>
 </div><!-- /card-avatar-content -->
