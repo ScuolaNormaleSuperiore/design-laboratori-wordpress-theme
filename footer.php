0647a4d3bd2742c4f65c1667e05cfa0c81904726
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2023-02-04 20:18:04 +0100

    Added mockup footer

--- footer.php
+++ footer.php
@@ -9,92 +9,164 @@
  * @package Design_Laboratori_Italia
  */
 ?>
-<footer id="footer-wrapper" class="footer-wrapper">
-    <div class="container">
-        <div class="row variable-gutters mb-5">
-            <div class="col logos-wrapper">
-                <img class="ue-logo"
-                    src="<?php echo esc_url( get_template_directory_uri() . '/assets/img/logo-eu-inverted.svg' ); ?>"
-                    alt="logo Unione Europea"
-                >
-                <div class="logo-footer">
-                    <?php get_template_part("template-parts/common/logo"); ?>
 
-                    <h2 class="h1">
-                        <a href="<?php echo home_url(); ?>">
-                            <span><?php echo dli_get_option("tipologia_scuola"); ?></span>
-                            <span><strong><?php echo dli_get_option("nome_laboratorio"); ?></strong></span>
-                            <span><?php echo dli_get_option("luogo_laboratorio"); ?></span>
-                        </a>
-                    </h2>
-                </div><!-- /logo-footer -->
-            </div><!-- /col -->
-        </div><!-- /row -->
-        <div class="row variable-gutters mb-3">
-            <div class="col-lg-3">
-                <?php dynamic_sidebar( 'footer-1' ); ?>
-            </div><!-- /col-lg-3 -->
-            <div class="col-lg-3">
-                <?php dynamic_sidebar( 'footer-2' ); ?>
-            </div><!-- /col-lg-3 -->
-
-            <div class="col-lg-3">
-                <?php dynamic_sidebar( 'footer-3' ); ?>
-            </div><!-- /col-lg-3 -->
-
-            <div class="col-lg-3">
-                <?php dynamic_sidebar( 'footer-4' ); ?>
-            </div><!-- /col-lg-3 -->
-        </div><!-- /row -->
-
-        <div class="row variable-gutters">
-            <div class="col-lg-12 sub-footer">
-                <?php
-                $location = "menu-footer";
-                if ( has_nav_menu( $location ) ) {
-                    wp_nav_menu(array(
-                        "theme_location" => $location, 
-                        "depth" => 1, 
-                        "menu_class" => "footer-inline-menu", 
-                        "container" => "",
-                        'walker' => new Footer_Menu_Walker()
-                    ));
-                }
-                ?>
-                <?php
-                $show_socials = dli_get_option( "show_socials", "socials" );
-                if($show_socials == "true") : ?>
-                    <div class="footer-social">
-                        <span>Seguici su:</span>
-                        <div class="footer-social-wrapper">
-                            <?php if($facebook = dli_get_option( "facebook", "socials" )) :?><a href="<?php echo $facebook; ?>" aria-label="facebook" title="vai alla pagina facebook"><svg class="icon it-social-facebook"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-facebook"></use></svg></a><?php endif; ?>
-                            <?php if($youtube = dli_get_option( "youtube", "socials" )) :?><a href="<?php echo $youtube; ?>" aria-label="youtube" title="vai alla pagina youtube"><svg class="icon it-social-youtube"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-youtube"></use></svg></a><?php endif; ?>
-                            <?php if($instagram = dli_get_option( "instagram", "socials" )) :?><a href="<?php echo $instagram; ?>" aria-label="instagram" title="vai alla pagina instagram"><svg class="icon it-social-instagram"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-instagram"></use></svg></a><?php endif; ?>
-                            <?php if($twitter = dli_get_option( "twitter", "socials" )) :?><a href="<?php echo $twitter; ?>" aria-label="twitter" title="vai alla pagina twitter"><svg class="icon it-social-twitter"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-twitter"></use></svg></a><?php endif; ?>
-                            <?php if($linkedin = dli_get_option( "linkedin", "socials" )) :?><a href="<?php echo $linkedin; ?>" aria-label="linkedin" title="vai alla pagina linkedin"><svg class="icon it-social-linkedin"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-linkedin"></use></svg></a><?php endif; ?>
-                        </div><!-- /footer-social-wrapper -->
-                    </div><!-- /gooter-social -->
-                <?php endif ?>
-            </div>
-        </div><!-- /row -->
-        <?php
-        $footer_text = dli_get_option("footer_text", "setup");
-        if(isset($footer_text) && trim($footer_text) != "") {
-            ?>
-            <div class="row variable-gutters mb-3">
-                <div class="col-lg-12 text-left text-md-center footer-text">
-                    <?php echo wpautop($footer_text); ?>
-                </div>
-            </div>
-            <?php
-        }
-        get_template_part("template-parts/common/copy");
-        ?>
-
+<!-- START FOOTER -->
+<footer class="it-footer">
+	<div class="it-footer-main">
+		<div class="container">
+			<section>
+				<div class="row clearfix">
+		<div class="col-sm-12">
+			<div class="it-brand-wrapper">
+				<a href="#">
+					<svg class="icon">
+			<use xlink:href="bootstrap-italia/svg/sprites.svg#it-code-circle"></use>
+					</svg>
+					<div class="it-brand-text">
+			<h2 class="no_toc">Lorem Ipsum</h2>
+			<h3 class="no_toc d-none d-md-block">Inserire qui la tag line</h3>
+					</div>
+				</a>
+			</div>
+		</div>
+				</div>
+			</section>
+			<section>
+				<div class="row">
+		<div class="col-lg-3 col-md-3 col-sm-6 pb-2">
+			<h4>
+				<a href="#" title="Vai alla pagina: Amministrazione">Amministrazione</a>
+			</h4>
+			<div class="link-list-wrapper">
+				<ul class="footer-list link-list clearfix">
+					<li><a class="list-item" href="#" title="Vai alla pagina: Giunta e consiglio">Giunta e consiglio</a>
+					</li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Aree di competenza">Aree di competenza</a>
+					</li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Dipendenti">Dipendenti</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Luoghi">Luoghi</a></li>
+					<li><a class="list-item" href="#"
+				title="Vai alla pagina: Associazioni e società partecipate">Associazioni e società partecipate</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6 pb-2">
+			<h4>
+				<a href="#" title="Vai alla pagina: Servizi">Servizi</a>
+			</h4>
+			<div class="link-list-wrapper">
+				<ul class="footer-list link-list clearfix">
+					<li><a class="list-item" href="#" title="Vai alla pagina: Pagamenti">Pagamenti</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Sostegno">Sostegno</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Domande e iscrizioni">Domande e
+				iscrizioni</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Segnalazioni">Segnalazioni</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Autorizzazioni e concessioni">Autorizzazioni
+				e concessioni</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Certificati e dichiarazioni">Certificati e
+				dichiarazioni</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6 pb-2">
+			<h4>
+				<a href="#" title="Vai alla pagina: Novità">Novità</a>
+			</h4>
+			<div class="link-list-wrapper">
+				<ul class="footer-list link-list clearfix">
+					<li><a class="list-item" href="#" title="Vai alla pagina: Notizie">Notizie</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Eventi">Eventi</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Comunicati stampa">Comunicati stampa</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-3 col-md-3 col-sm-6">
+			<h4>
+				<a href="#" title="Vai alla pagina: Documenti">Documenti</a>
+			</h4>
+			<div class="link-list-wrapper">
+				<ul class="footer-list link-list clearfix">
+					<li><a class="list-item" href="#" title="Vai alla pagina: Progetti e attività">Progetti e attività</a>
+					</li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Delibere, determine e ordinanze">Delibere,
+				determine e ordinanze</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Bandi">Bandi</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Concorsi">Concorsi</a></li>
+					<li><a class="list-item" href="#" title="Vai alla pagina: Albo pretorio">Albo pretorio</a></li>
+				</ul>
+			</div>
+		</div>
+				</div>
+			</section>
+			<section class="py-4 border-white border-top">
+				<div class="row">
+		<div class="col-lg-4 col-md-4 pb-2">
+			<h4><a href="#" title="Vai alla pagina: Contatti">Contatti</a></h4>
+			<p>
+				<strong>Comune di Lorem Ipsum</strong><br />
+				Via Roma 0 - 00000 Lorem Ipsum Codice fiscale / P. IVA: 000000000
+			</p>
+			<div class="link-list-wrapper">
+				<ul class="footer-list link-list clearfix">
+					<li><a class="list-item" href="#" title="Vai alla pagina: Posta Elettronica Certificata">Posta
+				Elettronica Certificata</a></li>
+					<li>
+			<a class="list-item" href="#" title="Vai alla pagina: URP - Ufficio Relazioni con il Pubblico">URP -
+				Ufficio Relazioni con il Pubblico</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="col-lg-4 col-md-4 pb-2">
+			<h4><a href="#" title="Vai alla pagina: Lorem Ipsum">Lorem Ipsum</a></h4>
+		</div>
+		<div class="col-lg-4 col-md-4 pb-2">
+			<div class="pb-2">
+				<h4><a href="#" title="Vai alla pagina: Seguici su">Seguici su</a></h4>
+				<ul class="list-inline text-left social">
+					<li class="list-inline-item">
+			<a class="p-2 text-white" href="#" target="_blank"><svg class="icon icon-sm icon-white align-top">
+					<use xlink:href="bootstrap-italia/svg/sprites.svg#it-designers-italia"></use>
+				</svg><span class="visually-hidden">Designers Italia</span></a>
+					</li>
+					<li class="list-inline-item">
+			<a class="p-2 text-white" href="#" target="_blank"><svg class="icon icon-sm icon-white align-top">
+					<use xlink:href="bootstrap-italia/svg/sprites.svg#it-twitter"></use>
+				</svg><span class="visually-hidden">Twitter</span></a>
+					</li>
+					<li class="list-inline-item">
+			<a class="p-2 text-white" href="#" target="_blank"><svg class="icon icon-sm icon-white align-top">
+					<use xlink:href="bootstrap-italia/svg/sprites.svg#it-medium"></use>
+				</svg><span class="visually-hidden">Medium</span></a>
+					</li>
+					<li class="list-inline-item">
+			<a class="p-2 text-white" href="#" target="_blank"><svg class="icon icon-sm icon-white align-top">
+					<use xlink:href="bootstrap-italia/svg/sprites.svg#it-behance"></use>
+				</svg><span class="visually-hidden">Behance</span></a>
+					</li>
+				</ul>
+			</div>
+		</div>
+				</div>
+			</section>
+		</div>
+	</div>
+	<div class="it-footer-small-prints clearfix">
+		<div class="container">
+			<h3 class="visually-hidden">Sezione Link Utili</h3>
+			<ul class="it-footer-small-prints-list list-inline mb-0 d-flex flex-column flex-md-row">
+				<li class="list-inline-item"><a href="#" title="Note Legali">Media policy</a></li>
+				<li class="list-inline-item"><a href="#" title="Note Legali">Note legali</a></li>
+				<li class="list-inline-item"><a href="#" title="Privacy-Cookies">Privacy policy</a></li>
+				<li class="list-inline-item"><a href="#" title="Mappa del sito">Mappa del sito</a></li>
+			</ul>
+		</div>
+	</div>
 </footer>
-</div><!-- /push_container -->
+<!-- END FOOTER -->
 
 <?php wp_footer(); ?>
-
 </body>
 </html>
