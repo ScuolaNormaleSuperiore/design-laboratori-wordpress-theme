c5c7cd307317291e4384d497cf313e7d884ef99e
Marcella Monreale <marcella.monreale@sns.it>
2023-02-21 13:44:25 +0100

    Inserimento form

--- page-templates/pubblicazioni.php
+++ page-templates/pubblicazioni.php
@@ -9,168 +9,168 @@ $anni_pubblicazioni = $wpdb->get_results( "SELECT DISTINCT meta_value FROM $wpdb
 
 ?>
 
-
-<main id="main-container">
-	<!-- BREADCRUMB -->
-	<section id ="breadcrumb">
-		<div class="container">
-			<div class="row">
-				<div class="col-12 ms-4 ">
-					<nav class="breadcrumb-container" aria-label="Percorso di navigazione">
-						<ol class="breadcrumb pb-0">
-							<li class="breadcrumb-item"><a href="<?php echo esc_url( get_site_url() ); ?>">Home</a><span class="separator">&gt;</span></li>
-							<li class="breadcrumb-item active" aria-current="<?php _e( 'Elenco pubblicazioni', 'design_laboratori_italia' ); ?>"><?php _e( 'Pubblicazioni', 'design_laboratori_italia' ); ?></li>
-						</ol>
-					</nav>
+<form action="<?php $_SERVER['PHP_SELF']; ?>" id="pubblicazioniform" method="GET">
+	<main id="main-container">
+		<!-- BREADCRUMB -->
+		<section id ="breadcrumb">
+			<div class="container">
+				<div class="row">
+					<div class="col-12 ms-4 ">
+						<nav class="breadcrumb-container" aria-label="Percorso di navigazione">
+							<ol class="breadcrumb pb-0">
+								<li class="breadcrumb-item"><a href="<?php echo esc_url( get_site_url() ); ?>">Home</a><span class="separator">&gt;</span></li>
+								<li class="breadcrumb-item active" aria-current="<?php _e( 'Elenco pubblicazioni', 'design_laboratori_italia' ); ?>"><?php _e( 'Pubblicazioni', 'design_laboratori_italia' ); ?></li>
+							</ol>
+						</nav>
+					</div>
 				</div>
 			</div>
-		</div>
-	</section>
-	<!-- BANNER PUBBLICAZIONI -->
-	<?php get_template_part( 'template-parts/hero/pubblicazioni' ); ?>
+		</section>
+		<!-- BANNER PUBBLICAZIONI -->
+		<?php get_template_part( 'template-parts/hero/pubblicazioni' ); ?>
 
-	<!-- ELENCO PUBBLICAZIONI -->
-	<section id="pubblicazioni" class="p-4">   
-		<div class="container my-4"> 
-			<div class="row pt-0"> <!-- SPAZIATURA ridotta in alto solo sulla prima riga riga pt-0 le card NON uniformate in altezza -->
-				<div class="col-12 col-lg-3 border-end">
-					<!--COLONNA FILTRI -->
-					<!-- FILTRO PER ANNO -->
-					<div class="row pt-3">
-						<h3 class="h6 text-uppercase border-bottom"><?php _e( 'Anno', 'design_laboratori_italia' ); ?></h3>
-						<div class="select-wrapper">
-							<label for="defaultSelect" class="visually-hidden"><?php _e( 'Anno', 'design_laboratori_italia' ); ?></label>
-							<select id="defaultSelect">
-								<option selected="" value=""><?php _e( 'Scegli un\'opzione', 'design_laboratori_italia' ); ?></option>
-								<?php
-								foreach ( $anni_pubblicazioni as $anno ) { ?>
-									<option value=""><?php echo $anno->meta_value; ?></option>
+		<!-- ELENCO PUBBLICAZIONI -->
+		<section id="pubblicazioni" class="p-4">   
+			<div class="container my-4"> 
+				<div class="row pt-0"> <!-- SPAZIATURA ridotta in alto solo sulla prima riga riga pt-0 le card NON uniformate in altezza -->
+					<div class="col-12 col-lg-3 border-end">
+						<!--COLONNA FILTRI -->
+						<!-- FILTRO PER ANNO -->
+						<div class="row pt-3">
+							<h3 class="h6 text-uppercase border-bottom"><?php _e( 'Anno', 'design_laboratori_italia' ); ?></h3>
+							<div class="select-wrapper">
+								<label for="defaultSelect" class="visually-hidden"><?php _e( 'Anno', 'design_laboratori_italia' ); ?></label>
+								<select id="defaultSelect">
+									<option selected="" value=""><?php _e( 'Scegli un\'opzione', 'design_laboratori_italia' ); ?></option>
+									<?php
+									foreach ( $anni_pubblicazioni as $anno ) { ?>
+										<option value=""><?php echo $anno->meta_value; ?></option>
+										<?php
+									}
+									?>
+								</select>
+							</div>
+						</div>
+						<?php
+						// recupero i termini della tassonomia tipo pubblicazione.
+						$tipi_pubblicazione = get_terms(
+							[
+								'taxonomy'   => 'tipo-pubblicazione',
+								'hide_empty' => false,
+							]
+						);
+						// visualizzo i filtri sul tipo doi pubblicazione solo se ne esistono almeno 1.
+						if ( count( $tipi_pubblicazione ) >= 1 ) {
+						?>
+						<!-- FILTRO PER CATEGORIA - Se esiste -->
+						<div class="row pt-5">
+							<h3 class="h6 text-uppercase border-bottom"><?php _e( 'Tipologia', 'design_laboratori_italia' ); ?></h3>
+							<div>
+								<?php foreach ( $tipi_pubblicazione as $tipo_pubblicazione ) { ?>
+								<div class="form-check">
+									<input id="checkbox4" type="checkbox" checked>
+									<label for="checkbox4"><?php echo esc_attr( $tipo_pubblicazione->name ); ?></label>
+								</div>
 									<?php
 								}
 								?>
-							</select>
-						</div>
-					</div>
-					<?php
-					// recupero i termini della tassonomia tipo pubblicazione.
-					$tipi_pubblicazione = get_terms(
-						[
-							'taxonomy'   => 'tipo-pubblicazione',
-							'hide_empty' => false,
-						]
-					);
-					// visualizzo i filtri sul tipo doi pubblicazione solo se ne esistono almeno 1.
-					if ( count( $tipi_pubblicazione ) >= 1 ) {
-					?>
-					<!-- FILTRO PER CATEGORIA - Se esiste -->
-					<div class="row pt-5">
-						<h3 class="h6 text-uppercase border-bottom"><?php _e( 'Tipologia', 'design_laboratori_italia' ); ?></h3>
-						<div>
-							<?php foreach ( $tipi_pubblicazione as $tipo_pubblicazione ) { ?>
-							<div class="form-check">
-								<input id="checkbox4" type="checkbox" checked>
-								<label for="checkbox4"><?php echo esc_attr( $tipo_pubblicazione->name ); ?></label>
 							</div>
-								<?php
-							}
-							?>
 						</div>
+							<?php
+						}
+						?>
+						<!--fine filtri -->
 					</div>
+
+					<!-- PUBBLICAZIONI -->
+					<?php
+					$pubblicazioni = new WP_Query(
+						array(
+							'posts_per_page' => -1,
+							'post_type'      => 'pubblicazione',
+							'orderby'        => 'anno',
+							'order'          => 'ASC',
+						)
+					);
+					if ( $pubblicazioni ) {
+					?>
+
+						<div class="col-12 col-lg-8">
+							<div class="row">
+								<?php
+								while ( $pubblicazioni->have_posts() ) {
+									$pubblicazioni->the_post();
+									$ID       = get_the_ID();
+									$title    = get_the_title( $ID );
+									$abstract = get_field( 'abstract' );
+									$url      = get_field( 'url' );
+									?>
+									<!--start card-->
+									<div class="card-wrapper pt-3">
+										<div class="card card-teaser rounded shadow">
+											<div class="card-body">
+												<?php
+												if ( $url ) {
+													?>
+													<h3 class="card-title h5">
+														<svg class="icon">
+															<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-note'; ?>"></use>
+														</svg>
+														<a href="<?php echo esc_url( $url ); ?>"><?php echo esc_attr( $title ); ?></a>
+													</h3>
+													<?php
+												}
+												else {
+													?>
+													<h3 class="card-title h5">
+														<svg class="icon">
+															<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-note'; ?>"></use>
+														</svg>
+														<?php echo esc_attr( $title ); ?>
+													</h3>
+												<?php } ?>
+												<p class="card-text"><?php echo esc_attr( $abstract ); ?></p>
+											</div>
+										</div>
+									</div>
+									<!--end card-->
+								<?php } ?>
+							</div>
+						</div>
 						<?php
 					}
 					?>
-					<!--fine filtri -->
 				</div>
-
-				<!-- PUBBLICAZIONI -->
-				<?php
-				$pubblicazioni = new WP_Query(
-					array(
-						'posts_per_page' => -1,
-						'post_type'      => 'pubblicazione',
-						'orderby'        => 'anno',
-						'order'          => 'ASC',
-					)
-				);
-				if ( $pubblicazioni ) {
-				?>
-
-					<div class="col-12 col-lg-8">
-						<div class="row">
-							<?php
-							while ( $pubblicazioni->have_posts() ) {
-								$pubblicazioni->the_post();
-								$ID       = get_the_ID();
-								$title    = get_the_title( $ID );
-								$abstract = get_field( 'abstract' );
-								$url = get_field( 'url' );
-								?>
-								<!--start card-->
-								<div class="card-wrapper pt-3">
-									<div class="card card-teaser rounded shadow">
-										<div class="card-body">
-											<?php
-											if ($url) {
-												?>
-												<h3 class="card-title h5">
-													<svg class="icon">
-														<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-note';?>"></use>
-													</svg>
-													<a href="<?php echo esc_url( $url ); ?>"><?php echo esc_attr( $title ); ?></a>
-												</h3>
-												<?php
-											}
-											else {
-												?>
-												<h3 class="card-title h5">
-													<svg class="icon">
-														<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-note';?>"></use>
-													</svg>
-													<?php echo esc_attr( $title ); ?>
-												</h3>
-											<?php } ?>
-											<p class="card-text"><?php echo esc_attr( $abstract ); ?></p>
-										</div>
-									</div>
-								</div>
-								<!--end card-->
-							<?php } ?>
-						</div>
-					</div>
-					<?php
-				}
-				?>
 			</div>
-		</div>
-	</section>
+		</section>
 
-	<!-- PAGINAZIONE -->
-<nav class="pagination-wrapper justify-content-center" aria-label="Navigazione centrata">
-              <ul class="pagination">
-                <li class="page-item disabled">
-                  <a class="page-link" href="#" tabindex="-1" aria-hidden="true">
-                    <svg class="icon icon-primary"><use href="bootstrap-italia/svg/sprites.svg#it-chevron-left"></use></svg>
-                    <span class="visually-hidden">Pagina precedente</span>
-                  </a>
-                </li>
-                <li class="page-item">
-                  <a class="page-link" href="#" aria-current="page">
-                    <span class="d-inline-block d-sm-none">Pagina </span>1
-                  </a>
-                </li>
-                <li class="page-item"><a class="page-link" href="#">2</a></li>
-                <li class="page-item"><a class="page-link" href="#">3</a></li>
-                <li class="page-item">
-                  <a class="page-link" href="#">
-                    <span class="visually-hidden">Pagina successiva</span>
-                    <svg class="icon icon-primary"><use href="bootstrap-italia/svg/sprites.svg#it-chevron-right"></use></svg>
-                  </a>
-                </li>
-              </ul>
-            </nav>
-    
-    </main>
-		   <!-- END CONTENT -->
+		<!-- PAGINAZIONE -->
+		<nav class="pagination-wrapper justify-content-center" aria-label="Navigazione centrata">
+			<ul class="pagination">
+				<li class="page-item disabled">
+					<a class="page-link" href="#" tabindex="-1" aria-hidden="true">
+						<svg class="icon icon-primary"><use href="bootstrap-italia/svg/sprites.svg#it-chevron-left"></use></svg>
+						<span class="visually-hidden">Pagina precedente</span>
+					</a>
+				</li>
+				<li class="page-item">
+					<a class="page-link" href="#" aria-current="page">
+						<span class="d-inline-block d-sm-none">Pagina </span>1
+					</a>
+				</li>
+				<li class="page-item"><a class="page-link" href="#">2</a></li>
+				<li class="page-item"><a class="page-link" href="#">3</a></li>
+				<li class="page-item">
+					<a class="page-link" href="#">
+						<span class="visually-hidden">Pagina successiva</span>
+						<svg class="icon icon-primary"><use href="bootstrap-italia/svg/sprites.svg#it-chevron-right"></use></svg>
+					</a>
+				</li>
+			</ul>
+		</nav>
+	</main>
+</form>
+<!-- END CONTENT -->
 
 <?php
 get_footer();
