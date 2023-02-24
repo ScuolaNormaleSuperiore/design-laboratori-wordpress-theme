fafc90b61168ab9ff87c3c80d6be0062db08bb51
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2023-02-24 16:32:10 +0100

    Aggiunta sezione contatti

--- template-parts/common/sezione-contatti.php
+++ template-parts/common/sezione-contatti.php
@@ -2,63 +2,71 @@
 	$items       = $args['items'];
 	$section_id  = $args['section_id'];
 	$num_results = is_array( $items ) ? count( $items ) : 0;
-	$phone       = $items[ 'phone' ];
-	$email       = $items[ 'email' ];
-	$website     = $items[ 'website' ];
+	$phone       = $items['phone'];
+	$email       = $items['email'];
+	$website     = $items['website'];
 ?>
 <section id="<?php echo 'sezione-' . $section_id; ?>">
-	<div class="it-list-wrapper">
-		<ul class="it-list">
-		<?php
-			if ( $phone ) {
-		?>
-		<li>
-			<div class="list-item">
+	<?php
+	if ( $phone || $email || $website ) {
+	?>
+		<div class="it-list-wrapper">
+			<ul class="it-list">
+			<?php
+				if ( $phone ) {
+			?>
+			<li>
+				<div class="list-item">
+					<div class="it-rounded-icon">
+						<svg class="icon">
+							<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-telephone'; ?>"></use>
+						</svg>
+					</div>
+					<div class="it-right-zone">
+						<span class="text"><?php echo esc_attr( $phone ); ?></span>
+					</div>
+				</div>
+			</li>
+			<?php
+				}
+				if ( $email ) {
+			?>
+			<li>
+				<a href="#" class="list-item">
 				<div class="it-rounded-icon">
-					<svg class="icon">
-						<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-telephone'; ?>"></use>
-					</svg>
+				<svg class="icon">
+					<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-mail'; ?>"></use>
+				</svg>
 				</div>
 				<div class="it-right-zone">
-					<span class="text">050 509662</span>
+					<span class="text"><?php echo esc_attr( $email ); ?></span>
 				</div>
-			</div>
-		</li>
-		<?php
-			}
-			if ( $email ) {
-		?>
-		<li>
-			<a href="#" class="list-item">
-			<div class="it-rounded-icon">
-			<svg class="icon">
-				<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-mail'; ?>"></use>
-			</svg>
-			</div>
-			<div class="it-right-zone">
-				<span class="text">mail@sns.it</span>
-			</div>
-			</a>
-		</li>
-		<?php
-			}
-			if ( $website ) {
-		?>
-		<li>
-			<a class="list-item" href="#">
-			<div class="it-rounded-icon">
-			<svg class="icon">
-				<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-link'; ?>"></use>
-			</svg>
-			</div>
-			<div class="it-right-zone">
-				<span class="text">www.sns.it</span>
-			</div>
-			</a>
-		</li>
-		<?php
-			}
-		?>
-		</ul>
-	</div>
+				</a>
+			</li>
+			<?php
+				}
+				if ( $website ) {
+			?>
+			<li>
+				<a class="list-item" href="#">
+				<div class="it-rounded-icon">
+				<svg class="icon">
+					<use href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-link'; ?>"></use>
+				</svg>
+				</div>
+				<div class="it-right-zone">
+					<span class="text"><?php echo esc_url( $website ); ?></span>
+				</div>
+				</a>
+			</li>
+			<?php
+				}
+			?>
+			</ul>
+		</div>
+	<?php
+	} else {
+		echo '<p>-</p>';
+	}
+	?>
 </section>
