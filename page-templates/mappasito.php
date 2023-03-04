e2b330d4c5bb3693c66e02973e18b460c74930fc
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2023-03-04 16:47:21 +0100

    mc

---
+++ page-templates/mappasito.php
@@ -0,0 +1,19 @@
+<?php
+/* Template Name: Notizie.
+ *
+ * @package Design_Laboratori_Italia
+ */
+get_header();
+
+?>
+
+<main id="main-container" class="main-container bluelectric">
+	<!-- BREADCRUMB -->
+	<?php get_template_part( 'template-parts/common/breadcrumb' ); ?>
+	
+	<h2>Questa è la pagina con la mappa del sito</h2>
+	
+</main>
+
+<?php
+get_footer();
