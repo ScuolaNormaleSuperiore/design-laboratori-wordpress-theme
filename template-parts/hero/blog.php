f4410111658324a736bfb2d6ce4ed97186171659
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2023-03-10 15:34:42 +0100

    Aggiunta visualizzazione blog

---
+++ template-parts/hero/blog.php
@@ -0,0 +1,14 @@
+<?php
+	$testo_sezione = dli_get_option( 'testo_blog', 'blog');
+?>
+<section id="banner-progetti" aria-describedby="Testo introduttivo sezione notizie" class="bg-banner-progetti">
+	<div class="section-muted p-3 primary-bg-c1">
+		<div class="container">
+			<div class="hero-title text-left ms-4 pb-3 pt-3">
+				<h2 class="p-0  "><?php echo __( 'Blog', 'design_laboratori_italia' ); ?></h2>
+				<p class="font-weight-normal"><?php echo esc_html( $testo_sezione ); ?></p>
+			</div>
+		</div>
+	</div>
+</section>
+
