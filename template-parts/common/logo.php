df4192c8509bbb9f82a182448c599379397ebe4b
Marcella Monreale <marcella.monreale@sns.it>
2023-03-24 17:01:36 +0100

    Fix logo in versione mobile che veniva tagliato

--- template-parts/common/logo.php
+++ template-parts/common/logo.php
@@ -2,14 +2,10 @@
 if ( dli_get_option( "logo_laboratorio" ) ) {
 ?>
 
-	<svg width="82" height="82" class="icon" aria-hidden="true">
-		<image xlink:href="<?php echo dli_get_option( 'logo_laboratorio' ); ?>" width="82" height="82"/>
-	</svg>
+	<image src="<?php echo dli_get_option( 'logo_laboratorio' ); ?>" xlink:href="<?php echo dli_get_option( 'logo_laboratorio' ); ?>" height="80"/>
 
 <?php } else { ?>
 
-	<svg width="82" height="82" class="icon" aria-hidden="true">
-		<image xlink:href="<?php echo dli_get_default_logo(); ?>" width="82" height="82"/>
-	</svg>
+	<image src="<?php echo dli_get_default_logo(); ?>" xlink:href="<?php echo dli_get_default_logo(); ?>" height="80"/>
 
 <?php } ?>
