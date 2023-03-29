93b1a4cd1307398811bda1ed4414df737cf2386e
Marcella Monreale <marcella.monreale@sns.it>
2023-03-29 08:29:27 +0200

    Aggiunti al e title per logo

--- template-parts/common/logo_footer.php
+++ template-parts/common/logo_footer.php
@@ -2,10 +2,10 @@
 if ( dli_get_option( "logo_laboratorio" ) ) {
 ?>
 
-	<img width="82"  class="color-invert" src="<?php echo dli_get_option( "logo_laboratorio" );?>" />
+	<img width="82"  class="color-invert" src="<?php echo dli_get_option( "logo_laboratorio" );?>" alt="<?php echo dli_get_option( 'nome_laboratorio' ); ?>" title="<?php echo dli_get_option( 'nome_laboratorio' ); ?>" />
 
 <?php } else { ?>
 
-	<img width="82" class="color-invert" src="<?php echo dli_get_default_logo(); ?>" />
+	<img width="82" class="color-invert" src="<?php echo dli_get_default_logo();?>" alt="<?php echo dli_get_option( 'nome_laboratorio' ); ?>" title="<?php echo dli_get_option( 'nome_laboratorio' ); ?>" />
 
 <?php } ?>
