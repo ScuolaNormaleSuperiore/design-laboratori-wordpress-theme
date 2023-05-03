8b565321c0cc736c3a36603b7c01ac58014d55a6
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2023-05-03 15:08:45 +0200

    Corretto layout mappa del sito

--- page-templates/mappasito.php
+++ page-templates/mappasito.php
@@ -28,51 +28,55 @@ try {
 
 	<!-- MAPPA DEL SITO -->
 	<div class="container my-4">
-		<ul class="menutree">
-			<li><a href="<?php echo $pt[DLI_HOMEPAGE_SLUG]['link']; ?>"><?php echo $pt[DLI_HOMEPAGE_SLUG]['name']; ?></a></li>
-			<ul>
-				<?php
-				// I livello.
-				foreach ( $pt[DLI_HOMEPAGE_SLUG]['children'] as $item ) {
-					if ( $item['external'] ) {
-						echo '<li><a target="_blank" href="' . $item['link'] . '">' . $item['name'] . '</a></li>';
-					} else {
-						echo '<li><a href="' . $item['link'] . '">' . $item['name'] . '</a></li>';
-					}
-					// II livello.
-					echo '<ul>';
-					foreach ( $item['children'] as $childitem ) {
-						if ( $childitem['external'] ) {
-							echo '<li><a target="_blank" href="' . $childitem['link'] . '">' . $childitem['name'] . '</a></li>';
-						} else {
-							echo '<li><a href="' . $childitem['link'] . '">' . $childitem['name'] . '</a></li>';
-						}
-						// III livello.
-						echo '<ul>';
-						foreach ( $childitem['children'] as $grandchilditem ) {
-							if ( $grandchilditem['external'] ) {
-								echo '<li><a target="_blank" href="' . $grandchilditem['link'] . '">' . $grandchilditem['name'] . '</a></li>';
+		<div class="row variable-gutters d-flex justify-content-center">
+			<div class="col-lg-8 pt84">
+				<ul class="menutree">
+					<li><a href="<?php echo $pt[DLI_HOMEPAGE_SLUG]['link']; ?>"><?php echo $pt[DLI_HOMEPAGE_SLUG]['name']; ?></a></li>
+					<ul>
+						<?php
+						// I livello.
+						foreach ( $pt[DLI_HOMEPAGE_SLUG]['children'] as $item ) {
+							if ( $item['external'] ) {
+								echo '<li><a target="_blank" href="' . $item['link'] . '">' . $item['name'] . '</a></li>';
 							} else {
-								echo '<li><a href="' . $grandchilditem['link'] . '">' . $grandchilditem['name'] . '</a></li>';
+								echo '<li><a href="' . $item['link'] . '">' . $item['name'] . '</a></li>';
 							}
+							// II livello.
+							echo '<ul>';
+							foreach ( $item['children'] as $childitem ) {
+								if ( $childitem['external'] ) {
+									echo '<li><a target="_blank" href="' . $childitem['link'] . '">' . $childitem['name'] . '</a></li>';
+								} else {
+									echo '<li><a href="' . $childitem['link'] . '">' . $childitem['name'] . '</a></li>';
+								}
+								// III livello.
+								echo '<ul>';
+								foreach ( $childitem['children'] as $grandchilditem ) {
+									if ( $grandchilditem['external'] ) {
+										echo '<li><a target="_blank" href="' . $grandchilditem['link'] . '">' . $grandchilditem['name'] . '</a></li>';
+									} else {
+										echo '<li><a href="' . $grandchilditem['link'] . '">' . $grandchilditem['name'] . '</a></li>';
+									}
+								}
+								echo '</ul>';
+							}
+							echo '</ul>';
 						}
-						echo '</ul>';
-					}
-					echo '</ul>';
-				}
-				?>
-			</ul>
-		</ul>
-	</div>
+						?>
+					</ul>
+				</ul>
 
-	<div class="box_change_map_lang">
-		<?php
-			if ( 'it' === $lng_slug ) {
-				echo '<a href="' . get_site_url() . '/en/' . SLUG_MAPPA_SITO_EN . '">Site Map in English</a>';
-			} else {
-				echo '<a href="' . get_site_url() . '/' . SLUG_MAPPA_SITO_IT . '">Mappa del sito in Italiano</a>';
-			}
-		?>
+				<div class="box_change_map_lang">
+					<?php
+						if ( 'it' === $lng_slug ) {
+							echo '<a href="' . get_site_url() . '/en/' . SLUG_MAPPA_SITO_EN . '">Site Map in English</a>';
+						} else {
+							echo '<a href="' . get_site_url() . '/' . SLUG_MAPPA_SITO_IT . '">Mappa del sito in Italiano</a>';
+						}
+					?>
+				</div>
+			</div>
+		</div>
 	</div>
 
 </main>
