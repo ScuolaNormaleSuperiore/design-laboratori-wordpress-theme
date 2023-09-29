dec71f1d4a0aff8ebdacfd3840b8aacfdd5f2cac
Claudio <ilclaudio@gmail.com>
2023-09-29 14:01:56 +0200

    BugFixing: Corretta pagina Not Found

--- template-parts/header/metatags.php
+++ template-parts/header/metatags.php
@@ -10,15 +10,16 @@ $wrapper   = dli_get_post_wrapper( $post );
 $copyright     = $nome_lab;
 $resource_type = 'document';
 $charset       = 'text/html; charset=US-ASCII';
-$keywords      = preg_replace( "/[^a-zA-Z0-9\s]/", '', $wrapper['title'] . ' ' . $tagline );
-$description   = $wrapper['description'];
+$page_title    =  array_key_exists( 'title', $wrapper ) ? $wrapper['title']: '';
+$page_desc     =  array_key_exists( 'description', $wrapper ) ? $wrapper['description']: '';
+$keywords      = preg_replace( "/[^a-zA-Z0-9\s]/", '', $page_title . ' ' . $tagline );
 
 $analytics_text = dli_get_option( 'analytics_code', 'setup' );
 
 ?>
 
 <meta name="resource-type" content="<?php echo $resource_type; ?>" />
-<meta name="description" content="<?php echo $description; ?>" />
+<meta name="description" content="<?php echo $page_desc; ?>" />
 <meta name="copyright" content="<?php echo $nome_lab; ?>" />
 <meta name="keywords" content="<?php echo $keywords; ?>"/>
 
