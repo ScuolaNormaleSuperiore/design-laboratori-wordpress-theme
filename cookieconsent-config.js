70b19742c2ca6aec41f8dfea706864916b2dc243
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-09-02 12:49:05 +0200

    mc

--- cookieconsent-config.js
+++ cookieconsent-config.js
@@ -6,10 +6,25 @@ CookieConsent.run({
 
 	categories: {
 		necessary: {
-			enabled: true,  // this category is enabled by default
-			readOnly: true  // this category cannot be disabled
+			enabled: true,
+			readOnly: true,
 		},
-		analytics: {}
+		functional: {
+			enabled: false,
+			readOnly: false,
+		},
+		analytics: {
+			enabled: false,
+			readOnly: false,
+		},
+		performance: {
+			enabled: false,
+			readOnly: false,
+		},
+		advertisement: {
+			enabled: false,
+			readOnly: false,
+		},
 	},
 
 	language: {
@@ -35,16 +50,31 @@ CookieConsent.run({
 							title: 'Somebody said ... cookies?',
 							description: 'I want one!'
 						},
+
 						{
 							title: 'Strictly Necessary cookies',
-							description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
-							//this field will generate a toggle linked to the 'necessary' category
+							description: 'Necessary cookies are required to enable the basic features of this site, such as providing secure log-in or adjusting your consent preferences. These cookies do not store any personally identifiable data.',
 							linkedCategory: 'necessary'
 						},
 						{
-							title: 'Performance and Analytics',
-							description: 'These cookies collect information about how you use our website. All of the data is anonymized and cannot be used to identify you.',
+							title: 'Functional cookies',
+							description: 'Functional cookies help perform certain functionalities like sharing the content of the website on social media platforms, collecting feedback, and other third-party features.',
+							linkedCategory: 'functional'
+						},
+						{
+							title: 'Analytics cookies',
+							description: 'Analytical cookies are used to understand how visitors interact with the website. These cookies help provide information on metrics such as the number of visitors, bounce rate, traffic source, etc.',
 							linkedCategory: 'analytics'
+						},
+						{
+							title: 'Performance cookies',
+							description: 'Performance cookies are used to understand and analyze the key performance indexes of the website which helps in delivering a better user experience for the visitors.',
+							linkedCategory: 'performance'
+						},
+						{
+							title: 'Advertisement cookies',
+							description: 'Advertisement cookies are used to provide visitors with customized advertisements based on the pages you visited previously and to analyze the effectiveness of the ad campaigns.',
+							linkedCategory: 'advertisement'
 						},
 						{
 							title: 'More information',
@@ -73,14 +103,24 @@ CookieConsent.run({
 							description: 'Ne voglio uno!'
 						},
 						{
-							title: 'Cookies strettamente necessari',
-							description: 'These cookies are essential for the proper functioning of the website and cannot be disabled.',
+							title: 'Cookie strettamente necessari',
+							description: 'I cookie necessari sono richiesti per abilitare le funzionalità di base di questo sito, come fornire un accesso sicuro o modificare le preferenze di consenso. Questi cookie non memorizzano dati personali identificabili.',
 							linkedCategory: 'necessary'
 						},
 						{
-							title: 'Prestazioni e Analytics',
-							description: 'Questi cookie sono essenziali per il corretto funzionamento del sito web e non possono essere disattivati.',
+							title: 'Cookie funzionali',
+							description: 'I cookie funzionali aiutano a svolgere determinate funzionalità, come la condivisione del contenuto del sito web su piattaforme di social media, la raccolta di feedback e altre funzionalità di terze parti.',
+							linkedCategory: 'functional'
+						},
+						{
+							title: 'Cookie analitici',
+							description: 'I cookie analitici vengono utilizzati per comprendere come i visitatori interagiscono con il sito web. Questi cookie aiutano a fornire informazioni su parametri quali il numero di visitatori, il tasso di rimbalzo, la fonte di traffico, ecc.',
 							linkedCategory: 'analytics'
+						},
+						{
+							title: 'Cookie pubblicitari',
+							description: 'I cookie pubblicitari vengono utilizzati per fornire ai visitatori annunci pubblicitari personalizzati in base alle pagine visitate in precedenza e per analizzare l\'efficacia delle campagne pubblicitarie.',
+							linkedCategory: 'advertisement'
 						},
 						{
 							title: 'Maggiori informazioni',
