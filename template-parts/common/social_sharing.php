09c3b44016eb0598207b36a3f352b33c9b9cd022
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2024-09-25 10:03:12 +0200

    Corretta la gestione condivisione contenuti sui social

--- template-parts/common/social_sharing.php
+++ template-parts/common/social_sharing.php
@@ -1,6 +1,18 @@
 <?php
 global $post;
-$current_url = get_permalink();
+// $current_url = get_permalink();
+$og_data          = DLI_ContentsManager::get_og_data();
+$page_url         = $og_data->url;
+$enc_page_url     = urlencode( $page_url );
+$shared_title     = __( 'I\'m pleased to share the post', 'kk_writer_theme' ) . ' "' . $og_data->shared_title . '"';
+$enc_shared_title = urlencode( $shared_title );
+// Prepare complete url to share the page on many platforms.
+$fb_share_url = 'https://facebook.com/sharer/sharer.php?u=' . $enc_page_url;
+$tw_share_url = 'https://twitter.com/share?url=' . $enc_page_url .'&text=' . $enc_shared_title;
+$lk_share_url = 'https://www.linkedin.com/sharing/share-offsite/?mini=true&url=' . $enc_page_url;
+$wa_share_url = 'https://api.whatsapp.com/send?text=' . $enc_shared_title . ' ' . $enc_page_url;
+$gm_share_url = 'https://mail.google.com/mail/u/0/?ui=2&fs=1&tf=cm&su=' . $enc_shared_title . ' &body=' . $og_data->description . ' - ' . $og_data->url;
+$mm_share_url = 'mailto:?subject=' . $enc_shared_title . '&body=' . $og_data->description . ' - ' . $og_data->url;
 ?>
 
 <div class="dropdown d-inline">
@@ -16,36 +28,59 @@ $current_url = get_permalink();
 		<div class="link-list-wrapper">
 			<ul class="link-list">
 				<li>
-					<a class="list-item" href="https://facebook.com/sharer/sharer.php?u=<?php echo esc_url( $current_url ) ; ?>" target="_blank" rel="noopener" aria-label="Share on Facebook">
+					<a class="list-item"
+						href="<?php echo esc_url( $fb_share_url ); ?>"
+						target="_blank" rel="noopener"
+						aria-label="<?php echo __( 'Share on Facebook', 'kk_writer_theme' ); ?>">
 						<svg class="icon" aria-hidden="true" focusable="false">
 							<use xlink:href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-facebook'?>"></use>
 						</svg>
-						<span>Facebook</span>
+						<span class="display_block">Facebook</span>
 					</a>
 				</li>
 				<li>
-					<a class="list-item" href="https://twitter.com/share?url=<?php echo esc_url( $current_url ) ; ?>">
+					<a class="list-item"
+						href="<?php echo esc_url( $tw_share_url ); ?>"
+						target="_blank" rel="noopener noreferrer"
+						aria-label="<?php echo __( 'Share on Twitter', 'kk_writer_theme' ); ?>">
 						<svg class="icon" aria-hidden="true" focusable="false">
 							<use xlink:href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-twitter'?>"></use>
 						</svg>
 						<br/>
-						<span>Twitter</span>
+						<span class="display_block">Twitter</span>
 					</a>
 				</li>
 				<li>
-					<a class="list-item" href="http://www.linkedin.com/shareArticle?mini=true&amp;url#=">
+					<a class="list-item"
+						href="<?php echo esc_url( $lk_share_url ); ?>"
+						target="_blank" rel="noopener noreferrer"
+						aria-label="<?php echo __( 'Share on Linkedin', 'kk_writer_theme' ); ?>">
 						<svg class="icon" aria-hidden="true" focusable="false">
 							<use xlink:href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-linkedin'?>"></use>
 						</svg>
-						<span>Linkedin</span>
+						<span class="display_block">Linkedin</span>
 					</a>
 				</li>
 				<li>
-					<a class="list-item" href="whatsapp://send?text==url" target="_blank" rel="noopener" aria-label="Share on Whatsapp">
+					<a class="list-item"
+						href="<?php echo esc_url( $wa_share_url ); ?>"
+						target="_blank" rel="noopener noreferrer"
+						aria-label="<?php echo __( 'Share on WhatsApp', 'kk_writer_theme' ); ?>">
 						<svg class="icon" aria-hidden="true" focusable="false">
 							<use xlink:href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-whatsapp'?>"></use>
 						</svg>
-						<span>Whatsapp</span>
+						<span class="display_block">Whatsapp</span>
+					</a>
+				</li>
+				<li>
+					<a class="list-item"
+						href="<?php echo esc_url( $mm_share_url ); ?>"
+						target="_blank" rel="noopener noreferrer"
+						aria-label="<?php echo __( 'Share by e-mail', 'kk_writer_theme' ); ?>">
+						<svg class="icon" aria-hidden="true" focusable="false">
+							<use xlink:href="<?php echo get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-mail'?>"></use>
+						</svg>
+						<span class="display_block">E-mail</span>
 					</a>
 				</li>
 			</ul>
