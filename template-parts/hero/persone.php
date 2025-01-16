c65b68966a5226968179213e4d3f9b8e379b4dc7
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-01-16 15:12:30 +0100

    Tolta stringa laboratiorio

--- template-parts/hero/persone.php
+++ template-parts/hero/persone.php
@@ -1,14 +1,14 @@
 <?php
 $testo_sezione_persone = dli_get_configuration_field_by_lang( 'testo_sezione_persone', 'persone' );
-
 ?>
+
 <section id="banner-persone" aria-describedby="Testo introduttivo sezione persone" class="bg-banner-persone">
-			<div class="section-muted p-3 primary-bg-c1">
-				<div class="container">
-					<div class="hero-title text-left ms-4 pb-3 pt-3">
-						<h2 class="p-0  "><?php _e( 'Le persone', 'design_laboratori_italia' ); ?></h2>
-						<p class="font-weight-normal"><?php echo $testo_sezione_persone; ?></p>
-					</div>
-				</div>
+	<div class="section-muted p-3 primary-bg-c1">
+		<div class="container">
+			<div class="hero-title text-left ms-4 pb-3 pt-3">
+				<h2 class="p-0  "><?php _e( 'Le persone', 'design_laboratori_italia' ); ?></h2>
+				<p class="font-weight-normal"><?php echo $testo_sezione_persone; ?></p>
 			</div>
-		</section>
+		</div>
+	</div>
+</section>
