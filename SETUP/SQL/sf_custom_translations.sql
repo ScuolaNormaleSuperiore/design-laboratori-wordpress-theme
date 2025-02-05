c2513940f0e737b0ea2f48dbcc1e359ca04962bf
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-02-05 11:02:01 +0100

    Rinominata tabella traduzioni personalizzate

--- SETUP/SQL/sf_custom_translations.sql
+++ SETUP/SQL/sf_custom_translations.sql
@@ -1,4 +1,4 @@
-CREATE TABLE IF NOT EXISTS sf_custom_translations (
+CREATE TABLE IF NOT EXISTS wp_dli_custom_translations (
   id BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
   label TEXT NOT NULL,
   domain VARCHAR(100) NOT NULL,
