c5dab0b2aff69519033bc5605daf8be5fe42643a
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2025-05-19 15:50:41 +0200

    Refactoring: Rifattorizzata la query di ricerca nel sito e ordinati i risultati per titolo

--- inc/search-utils.php
+++ inc/search-utils.php
@@ -1,27 +1,19 @@
 <?php
 
 if ( ! function_exists( 'dli_main_search_query' ) ) {
-		function dli_main_search_query( $selected_contents, $searchstring, $pagesize ) {
-			if( count( $selected_contents ) > 0 ) {
-			$the_query = new WP_Query(
-				array(
-					'paged'          => get_query_var( 'paged', 1 ),
-					'post_type'      => $selected_contents,
-					'post_status'    => 'publish',
-					'posts_per_page' => $pagesize,
-					's'              => $searchstring,
-				)
-			);
-			}	else {
-				$the_query = new WP_Query(
-					array(
-						'paged'          => get_query_var( 'paged', 1 ),
-						'post_status'    => 'publish',
-						'posts_per_page' => $pagesize,
-						's'              => $searchstring,
-					)
-				);
-			}
+		function dli_main_search_query( $selected_contents, $search_string, $page_size ) {
+			$params = array(
+			'paged'          => get_query_var( 'paged', 1 ),
+			'post_status'    => 'publish',
+			'posts_per_page' => $page_size,
+			's'              => $search_string,
+			'orderby'        => 'title',
+			'order'          => 'ASC',
+		);
+		if( count( $selected_contents ) > 0 ) {
+			$params['post_type'] = $selected_contents;
+		}
+		$the_query = new WP_Query( $params );
 		return $the_query;
 	}
 }
