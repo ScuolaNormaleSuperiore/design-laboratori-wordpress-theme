fbf491d8c70ab619cdbee1d9c90ce66b0541b370
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2026-02-13 13:10:54 +0100

    Bug-fix: Remove conflicting charset meta declaration

--- template-parts/header/metatags.php
+++ template-parts/header/metatags.php
@@ -12,7 +12,6 @@ $dli_wrapper    = dli_get_post_wrapper( get_post() );
 
 $dli_copyright     = $dli_nome_lab;
 $dli_resource_type = 'document';
-$dli_charset       = 'text/html; charset=US-ASCII';
 $dli_page_title    = array_key_exists( 'title', $dli_wrapper ) ? $dli_wrapper['title'] : '';
 $dli_page_desc     = array_key_exists( 'description', $dli_wrapper ) ? $dli_wrapper['description'] : '';
 $dli_keywords      = preg_replace( '/[^a-zA-Z0-9\s]/', '', $dli_page_title . ' ' . $dli_tagline );
@@ -24,5 +23,4 @@ $dli_keywords      = preg_replace( '/[^a-zA-Z0-9\s]/', '', $dli_page_title . ' '
 <meta name="copyright" content="<?php echo esc_attr( $dli_copyright ); ?>" />
 <meta name="keywords" content="<?php echo esc_attr( $dli_keywords ); ?>"/>
 
-<meta http-equiv="content-type" content="<?php echo esc_attr( $dli_charset ); ?>" />
 <meta http-equiv="content-language" content="<?php echo esc_attr( $dli_locale_str ); ?>" />
