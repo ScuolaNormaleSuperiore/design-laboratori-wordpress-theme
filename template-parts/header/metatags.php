e70064cf9f3a2a5585c07624b4f4aa4cddad514e
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2026-02-13 12:44:05 +0100

    Refacroting: Corretto lo stile del file.

--- template-parts/header/metatags.php
+++ template-parts/header/metatags.php
@@ -1,24 +1,28 @@
 <?php
-global $post;
+/**
+ * Header meta tags output.
+ *
+ * @package Design_Laboratori_Italia
+ */
 
-$localestr = pll_current_language( 'locale' );
-$nome_lab  = dli_get_option_by_lang( 'nome_laboratorio' );
-$tagline   = dli_get_option_by_lang( 'tagline_laboratorio' );
-$wrapper   = dli_get_post_wrapper( $post );
+$dli_locale_str = pll_current_language( 'locale' );
+$dli_nome_lab   = dli_get_option_by_lang( 'nome_laboratorio' );
+$dli_tagline    = dli_get_option_by_lang( 'tagline_laboratorio' );
+$dli_wrapper    = dli_get_post_wrapper( get_post() );
 
-$copyright     = $nome_lab;
-$resource_type = 'document';
-$charset       = 'text/html; charset=US-ASCII';
-$page_title    =  array_key_exists( 'title', $wrapper ) ? $wrapper['title'] : '';
-$page_desc     =  array_key_exists( 'description', $wrapper ) ? $wrapper['description'] : '';
-$keywords      = preg_replace( "/[^a-zA-Z0-9\s]/", '', $page_title . ' ' . $tagline );
+$dli_copyright     = $dli_nome_lab;
+$dli_resource_type = 'document';
+$dli_charset       = 'text/html; charset=US-ASCII';
+$dli_page_title    = array_key_exists( 'title', $dli_wrapper ) ? $dli_wrapper['title'] : '';
+$dli_page_desc     = array_key_exists( 'description', $dli_wrapper ) ? $dli_wrapper['description'] : '';
+$dli_keywords      = preg_replace( '/[^a-zA-Z0-9\s]/', '', $dli_page_title . ' ' . $dli_tagline );
 
 ?>
 
-<meta name="resource-type" content="<?php echo esc_attr( $resource_type ); ?>" />
-<meta name="description" content="<?php echo esc_attr( $page_desc ); ?>" />
-<meta name="copyright" content="<?php echo esc_attr( $copyright ); ?>" />
-<meta name="keywords" content="<?php echo esc_attr( $keywords ); ?>"/>
+<meta name="resource-type" content="<?php echo esc_attr( $dli_resource_type ); ?>" />
+<meta name="description" content="<?php echo esc_attr( $dli_page_desc ); ?>" />
+<meta name="copyright" content="<?php echo esc_attr( $dli_copyright ); ?>" />
+<meta name="keywords" content="<?php echo esc_attr( $dli_keywords ); ?>"/>
 
-<meta http-equiv="content-type" content="<?php echo esc_attr( $charset ); ?>" />
-<meta http-equiv="content-language" content="<?php echo esc_attr( $localestr ); ?>" />
+<meta http-equiv="content-type" content="<?php echo esc_attr( $dli_charset ); ?>" />
+<meta http-equiv="content-language" content="<?php echo esc_attr( $dli_locale_str ); ?>" />
