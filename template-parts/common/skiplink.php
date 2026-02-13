f4b2318e808b88cb806fd046fd0c1f9bb804b1fe
Claudio BATTAGLINO <claudio.battaglino@sns.it>
2026-02-13 13:17:51 +0100

    Bug-fix: Fix invalid skiplink markup and escaping

--- template-parts/common/skiplink.php
+++ template-parts/common/skiplink.php
@@ -1,6 +1,14 @@
+<?php
+/**
+ * Skiplink partial.
+ *
+ * @package Design_Laboratori_Italia
+ */
+
+?>
+
 <div class="skiplink" role="region" aria-label="Skiplink">
-	<a class="visually-hidden-focusable" href="#main-container"><?php _e("Vai ai contenuti", 'design_laboratori_italia'); ?></a>
-	<a class="visually-hidden-focusable" href="#menu-principale"><?php _e("Vai al menu di navigazione", 'design_laboratori_italia'); ?></a>
-	<a class="visually-hidden-focusable" href="#footer-container"><?php _e("Vai al footer", 'design_laboratori_italia'); ?></a>
-	</ul>
-</div><!-- /skiplink -->
+	<a class="visually-hidden-focusable" href="#main-container"><?php esc_html_e( 'Vai ai contenuti', 'design_laboratori_italia' ); ?></a>
+	<a class="visually-hidden-focusable" href="#menu-principale"><?php esc_html_e( 'Vai al menu di navigazione', 'design_laboratori_italia' ); ?></a>
+	<a class="visually-hidden-focusable" href="#footer-container"><?php esc_html_e( 'Vai al footer', 'design_laboratori_italia' ); ?></a>
+</div>
