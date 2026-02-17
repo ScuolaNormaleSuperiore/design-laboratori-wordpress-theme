ca38990bf5c751669ac1f68ed65344f4d327adb4
Claudio Battaglino <claudio.battaglino@sns.it>
2026-02-17 10:50:01 +0100

    Security: sanitize analytics head snippet output

--- template-parts/header/analytics.php
+++ template-parts/header/analytics.php
@@ -5,5 +5,42 @@
  * @package Design_Laboratori_Italia
  */
 
-$analytics_text = dli_get_option( 'analytics_code', 'setup' );
-echo $analytics_text;
+$analytics_text = (string) dli_get_option( 'analytics_code', 'setup' );
+
+$allowed_analytics_html = array(
+	'script'   => array(
+		'src'             => true,
+		'type'            => true,
+		'async'           => true,
+		'defer'           => true,
+		'id'              => true,
+		'crossorigin'     => true,
+		'integrity'       => true,
+		'referrerpolicy'  => true,
+		'nonce'           => true,
+	),
+	'noscript' => array(),
+	'iframe'   => array(
+		'src'             => true,
+		'height'          => true,
+		'width'           => true,
+		'style'           => true,
+		'title'           => true,
+		'loading'         => true,
+		'referrerpolicy'  => true,
+	),
+	'img'      => array(
+		'src'             => true,
+		'alt'             => true,
+		'height'          => true,
+		'width'           => true,
+		'style'           => true,
+		'loading'         => true,
+		'decoding'        => true,
+		'referrerpolicy'  => true,
+	),
+);
+
+if ( '' !== $analytics_text ) {
+	echo wp_kses( $analytics_text, $allowed_analytics_html );
+}
