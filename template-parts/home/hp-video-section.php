5d63f1af690b8afba1585df832be8a5c2749ed97
Claudio Battaglino <claudio.battaglino@sns.it>
2026-02-17 15:54:54 +0100

    Fix SVG sprite paths for video sections

--- template-parts/home/hp-video-section.php
+++ template-parts/home/hp-video-section.php
@@ -1,74 +1,76 @@
 <?php
 /**
- * The HP Banners section.
+ * Homepage video section.
  *
  * @package Design_Laboratori_Italia
  */
 
-$enabled_par     = $args['enabled'] ?? '';
-$id_par          = $args['id'] ?? '';
-$show_title_par  = $args['show_title'] ?? '';
-$show_title      = ( $show_title_par === 'true' ) ? true : false;
-$section_enabled = ( $enabled_par === 'true' ) ? true : false;
-// Get the video URL.
-$video = dli_get_option( 'home_page_video_url', 'homepage' ) ?? null;
+$dli_enabled_par     = $args['enabled'] ?? '';
+$dli_show_title_par  = $args['show_title'] ?? '';
+$dli_show_title      = ( 'true' === $dli_show_title_par );
+$dli_section_enabled = ( 'true' === $dli_enabled_par );
+$dli_video_url       = dli_get_option( 'home_page_video_url', 'homepage' );
 
-if ( $section_enabled && $video ) {
+if ( $dli_section_enabled && $dli_video_url ) {
+	$dli_page_label       = __( 'Privacy Policy', 'design_laboratori_italia' );
+	$dli_current_language = dli_current_language( 'slug' );
+	$dli_page_link        = dli_get_privacy_link( $dli_current_language );
 
-$page_label       = __( 'Privacy Policy', 'design_laboratori_italia' );
-$current_language = dli_current_language( 'slug' );
-$page_link        = dli_get_privacy_link( $current_language );
-$msg_text         = sprintf( __( 'Accetta i cookie di Youtube per guardare il video. Puoi gestire le preferenze in <a target="_blank" href="%s" class="text-white">%s</a>.', 'design_laboratori_italia' ),
-	$page_link, 
-	$page_label
-);
-
-?>
+	$dli_message_text = sprintf(
+		/* translators: 1: Privacy page URL, 2: Privacy page label. */
+		__(
+			'Accetta i cookie di Youtube per guardare il video. Puoi gestire le preferenze in <a target="_blank" href="%1$s" class="text-white">%2$s</a>.',
+			'design_laboratori_italia'
+		),
+		esc_url( $dli_page_link ),
+		esc_html( $dli_page_label )
+	);
+	$dli_allowed_html = array(
+		'a' => array(
+			'target' => true,
+			'href'   => true,
+			'class'  => true,
+		),
+	);
+	?>
 
 	<div class="container-banner-home">
-
-		<!-- Title -->
-		<?php
-			if ( $show_title ) {
-		?>
+		<?php if ( $dli_show_title ) : ?>
 			<h2 class="pb-4">
-				<?php echo __( 'Video', 'design_laboratori_italia' );?>
+				<?php esc_html_e( 'Video', 'design_laboratori_italia' ); ?>
 			</h2>
-		<?php
-			}
-		?>
+		<?php endif; ?>
 
-		<!-- Body -->
 		<section class="section section-muted p-5">
 			<div class="row">
 				<div class="mx-auto col-12 col-sm-10 col-md-8 col-lg-6">
 					<script>
 						const loadYouTubeVideo = function (videoUrl) {
-							const videoEl = document.getElementById("vid1");
+							const videoEl = document.getElementById('vid1');
 							const video = bootstrap.VideoPlayer.getOrCreateInstance(videoEl);
 							video.setYouTubeVideo(videoUrl);
-						}
+						};
 					</script>
 					<div class="acceptoverlayable">
 						<div class="acceptoverlay acceptoverlay-primary fade show">
 							<div class="acceptoverlay-inner">
 								<div class="acceptoverlay-icon">
 									<svg class="icon icon-xl">
-										<use href="/bootstrap-italia/svg/sprites.svg#it-video"></use>
+										<use href="<?php echo esc_url( get_template_directory_uri() . '/assets/bootstrap-italia/svg/sprites.svg#it-video' ); ?>"></use>
 									</svg>
 								</div>
 								<p>
-									<?php echo $msg_text; ?>
+									<?php echo wp_kses( $dli_message_text, $dli_allowed_html ); ?>
 								</p>
 								<div class="acceptoverlay-buttons bg-dark">
 									<button type="button" class="btn btn-primary" data-bs-accept-from="youtube.com"
-										onclick="loadYouTubeVideo('<?php echo esc_url( $video ); ?>')">
-										<?php echo __( 'Accept', 'design_laboratori_italia' ); ?>
+										onclick="loadYouTubeVideo('<?php echo esc_url( $dli_video_url ); ?>')">
+										<?php esc_html_e( 'Accept', 'design_laboratori_italia' ); ?>
 									</button>
 									<div class="form-check">
 										<input id="chk-remember" type="checkbox" data-bs-accept-remember>
 										<label for="chk-remember">
-											<?php echo __( 'Ricorda per tutti i video', 'design_laboratori_italia' ); ?>
+											<?php esc_html_e( 'Ricorda per tutti i video', 'design_laboratori_italia' ); ?>
 										</label>
 									</div>
 								</div>
@@ -82,9 +84,7 @@ $msg_text         = sprintf( __( 'Accetta i cookie di Youtube per guardare il vi
 				</div>
 			</div>
 		</section>
-
 	</div>
 
-<?php
+	<?php
 }
-?>
